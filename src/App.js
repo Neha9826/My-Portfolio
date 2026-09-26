678f5bea23afe4d3fd93f3f0d034bea724a81918
Neha Pattnayak <pattnayakneha20@gmail.com>
2026-09-26 13:19:07 +0530

    fix: move about section before contact

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <About />
       <Skills />
       <Projects />
+      <About />
       <Contact />
       <Footer />
       <button
